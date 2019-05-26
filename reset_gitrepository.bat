de6a0212016a2db2b4e54bb8d48d0f9b165f373d
JackChen <jzaicn@163.com>
2019-05-26 13:22:06 +0800

    hard reset git repository

---
+++ reset_gitrepository.bat
@@ -0,0 +1,12 @@
+::尝试 运行 
+git checkout --orphan latest_branch
+::添加所有文件
+git add -A
+::提交更改
+git commit -am "hard reset git repository"
+::删除分支
+git branch -D master
+::将当前分支重命名
+git branch -m master
+::最后，强制更新存储库。
+git push -f origin master
